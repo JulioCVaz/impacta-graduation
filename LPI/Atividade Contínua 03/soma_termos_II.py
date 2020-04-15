3f674d9544a581686ef75403eb78949819d9b4e3
Júlio Vaz <julio.oliveiravaz0@gmail.com>
2020-04-14 23:20:34 -0300

    finalizado soma_termos_II

--- LPI/Atividade Contínua 03/soma_termos_II.py
+++ LPI/Atividade Contínua 03/soma_termos_II.py
@@ -4,3 +4,20 @@
 
 # DICA 2: Você pode criar uma variável para calcular as frações: 1/1, 1/2, 1/3, 1/4, ... e depois usar uma estrutura de decisão interna ao laço de repetição para decidir se aquela fração deve ser somada ou subtraída. Observe que a decisão de somar ou subtrair está associada à posição dos números: o 1°, 3°, 5°, 7°, ... são subtraídos e o 2°, 4°, 6°, 8°, ... são somados.
 
+n = int(input())
+
+denominador = 1
+
+nums = 0
+
+while(n):
+    if ( denominador % 2 == 0 ) :
+        nums += ( 1 / denominador)
+    else :
+        nums -= ( 1 / denominador)
+ 
+    denominador += 1
+
+    n -= 1
+
+print(f'{nums:.6f}')
